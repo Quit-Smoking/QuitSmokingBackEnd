b2cd4bd8f4ef6b4f3ecb8daacd06ec18a83833a5
Kokodaks <dayeonko357@gmail.com>
2024-10-29 16:36:56 +0900

    MissionController.java에서 불필요한 User 클래스 import를 제거

--- src/main/java/com/example/accessingdatamysql/Mission/MissionController.java
+++ src/main/java/com/example/accessingdatamysql/Mission/MissionController.java
@@ -1,7 +1,6 @@
 package com.example.accessingdatamysql.Mission;
 
 import com.example.accessingdatamysql.Security.JwtUtil;
-import com.example.accessingdatamysql.User.User;
 import com.example.accessingdatamysql.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
