cd3ffd31015cb428b54fff79bf644724e763df2f
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2024-11-19 16:16:09 +0900

    에러 수정 및 Service 파일 추가.

--- src/main/java/com/example/accessingdatamysql/CoupangCrawling/CoupangCrawlingController.java
+++ src/main/java/com/example/accessingdatamysql/CoupangCrawling/CoupangCrawlingController.java
@@ -1,48 +1,24 @@
 package com.example.accessingdatamysql.CoupangCrawling;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.example.accessingdatamysql.CoupangCrawling.SearchAnswer;
+import com.example.accessingdatamysql.CoupangCrawling.CoupangCrawlingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/coupang")
 public class CoupangCrawlingController {
-    @GetMapping(path = "/search")
-    public SearchAnswer searchProducts(@RequestParam String keyword, @RequestParam Integer page){
-        WebDriver driver = new ChromeDriver();
 
-        SearchAnswer searchAnswer = new SearchAnswer();
-        String url = "https://www.coupang.com/np/search?q=" + keyword + "&page=" + page;
-        try {
-            driver.get(url);
+    private final CoupangCrawlingService coupangCrawlingService;
 
-            List<WebElement> elements = driver.findElements(By.className("search-product"));
-            // element는 하나의 상품.
-            for(WebElement element : elements){
-                String productName = element.findElement(By.className("name")).getText();
-                String productPrice = element.findElement(By.className("price-value")).getText();
-                String productImageUrl = element.findElement(By.className("search-product-wrap-img")).getAttribute("src");
-                String productUrl = element.findElement(By.className("search-product-link")).getAttribute("href");
-                float rank = Float.parseFloat(element.findElement(By.className("rating")).getText());
-
-                ProductData product = new ProductData(productName, productPrice, productImageUrl, productUrl, rank);
-                searchAnswer.addProduct(product);
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return searchAnswer;
+    public CoupangCrawlingController(CoupangCrawlingService coupangCrawlingService) {
+        this.coupangCrawlingService = coupangCrawlingService;
     }
 
-    @Async
-    @GetMapping("/test")
-    public void test() {
+    @GetMapping(path = "/search")
+    public SearchAnswer searchProducts(@RequestParam String keyword, @RequestParam Integer page) {
+        return coupangCrawlingService.searchProducts(keyword, page);
     }
 }
