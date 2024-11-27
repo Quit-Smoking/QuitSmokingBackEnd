df759016d8efc887c54a89d9049bc84e7d23b5cb
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2024-11-27 18:47:08 +0900

    Reapply "CoupangCrawingService Chrome Driver 설정"
    This reverts commit 1c27a7f5729acc12e23cf5b65d0f6acfe088fab4.

--- src/main/java/com/example/accessingdatamysql/CoupangCrawling/CoupangCrawlingService.java
+++ src/main/java/com/example/accessingdatamysql/CoupangCrawling/CoupangCrawlingService.java
@@ -7,12 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -20,33 +16,28 @@ public class CoupangCrawlingService {
 
     public SearchAnswer searchProducts(String keyword, Integer page) {
 
-        //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
-        //options.addArguments("--disable-gpu");
-        //options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
 
 
-        WebDriver driver = null;
+        WebDriver driver = new ChromeDriver(options);
         SearchAnswer searchAnswer = new SearchAnswer();
         String url = "https://www.coupang.com/np/search?q=" + keyword + "&page=" + page;
 
         try {
-            driver = new ChromeDriver(options);
             driver.get(url);
-
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-            //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("search-product")));
 
             List<WebElement> elements = driver.findElements(By.className("search-product"));
             for (WebElement element : elements) {
-                String productName = getElementText(element, By.className("name"), wait);
-                String productPrice = getElementText(element, By.className("price-value"), wait);
-                String productImageUrl = getElementAttribute(element, By.className("search-product-wrap-img"), "src", wait);
-                String productUrl = getElementAttribute(element, By.className("search-product-link"), "href", wait);
-                float rank = getElementTextAsFloat(element, By.className("rating"), 0.0f, wait);
+                String productName = getElementText(element, By.className("name"));
+                String productPrice = getElementText(element, By.className("price-value"));
+                String productImageUrl = getElementAttribute(element, By.className("search-product-wrap-img"), "src");
+                String productUrl = getElementAttribute(element, By.className("search-product-link"), "href");
+                float rank = getElementTextAsFloat(element, By.className("rating"), 0.0f);
 
                 ProductData product = new ProductData(productName, productPrice, productImageUrl, productUrl, rank);
                 searchAnswer.addProduct(product);
@@ -54,38 +45,31 @@ public class CoupangCrawlingService {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if(driver != null)
-                driver.quit();
+            driver.quit();
         }
 
         return searchAnswer;
     }
 
-    private String getElementText(WebElement element, By by, WebDriverWait wait) {
-        try {
-            WebElement targetElement = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(element, by));
-            return targetElement.getText();
-        } catch (Exception e) {
-            return null;
-        }
-
+    private String getElementText(WebElement element, By by) {
+        List<WebElement> elements = element.findElements(by);
+        return elements.isEmpty() ? null : elements.get(0).getText();
     }
 
-    private String getElementAttribute(WebElement element, By by, String attribute, WebDriverWait wait) {
-        try {
-            WebElement targetElement = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(element, by));
-            return targetElement.getAttribute(attribute);
-        } catch (Exception e) {
-            return null;
-        }
+    private String getElementAttribute(WebElement element, By by, String attribute) {
+        List<WebElement> elements = element.findElements(by);
+        return elements.isEmpty() ? null : elements.get(0).getAttribute(attribute);
     }
 
-    private float getElementTextAsFloat(WebElement element, By by, float defaultValue, WebDriverWait wait) {
-        try {
-            WebElement targetElement = wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(element, by));
-            return Float.parseFloat(targetElement.getText());
-        } catch (Exception e) {
-            return defaultValue;
+    private float getElementTextAsFloat(WebElement element, By by, float defaultValue) {
+        List<WebElement> elements = element.findElements(by);
+        if (!elements.isEmpty()) {
+            try {
+                return Float.parseFloat(elements.get(0).getText());
+            } catch (NumberFormatException e) {
+                return defaultValue;
+            }
         }
+        return defaultValue;
     }
 }
