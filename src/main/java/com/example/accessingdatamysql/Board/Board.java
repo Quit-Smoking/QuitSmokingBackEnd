c387a6d5d09b4412d25d8645351274291306dcd7
Kokodaks <dayeonko357@gmail.com>
2024-11-28 01:08:00 +0900

    엔티티를 가지고 있는 Board.java 추가

--- src/main/java/com/example/accessingdatamysql/Board/Board.java
+++ src/main/java/com/example/accessingdatamysql/Board/Board.java
@@ -1,5 +1,36 @@
 package com.example.accessingdatamysql.Board;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
 public class Board {
-    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    private int userid;
+
+    private String content;
+
+    private LocalDate date;
+
+    public int getId(){
+        return id;
+    }
+
+    public int getUserid(){
+        return userid;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
 }
---
+++ src/main/java/com/example/accessingdatamysql/Board/BoardRepository.java
@@ -0,0 +1,5 @@
+package com.example.accessingdatamysql.Board;
+
+public class BoardRepository {
+
+}
