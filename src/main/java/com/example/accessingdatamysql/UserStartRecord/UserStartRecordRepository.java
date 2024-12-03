aa9f1ff6ba6416cffa349a9cd9082adb81db48c5
Kokodaks <dayeonko357@gmail.com>
2024-12-03 16:26:04 +0900

    충돌 해결

--- src/main/java/com/example/accessingdatamysql/UserStartRecord/UserStartRecordRepository.java
+++ src/main/java/com/example/accessingdatamysql/UserStartRecord/UserStartRecordRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserStartRecordRepository extends CrudRepository<UserStartRecord, Integer> {
     UserStartRecord findRecordByUserId(Integer userId);
     void deleteAllByUserId(Integer userId);
-
 }
