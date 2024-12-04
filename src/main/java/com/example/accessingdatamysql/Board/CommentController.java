b982485959e95450f5a9b85c9d55d1437cbbece5
Kokodaks <dayeonko357@gmail.com>
2024-12-04 22:48:44 +0900

    Unused imports 삭제

--- src/main/java/com/example/accessingdatamysql/Board/CommentController.java
+++ src/main/java/com/example/accessingdatamysql/Board/CommentController.java
@@ -2,7 +2,6 @@ package com.example.accessingdatamysql.Board;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
