a845db603d8a7bdc53ee4f6eb4260ec984fe2c2d
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2024-12-06 04:19:07 +0900

    update server config

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -1,8 +1,17 @@
 package com.example.accessingdatamysql;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://quitsmoking.co.kr", description = "Prod-Server"),
+		@Server(url = "http://localhost:8080", description = "Local-Server")
+})
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
 
