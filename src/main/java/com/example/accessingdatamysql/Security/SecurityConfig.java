37f0fd0a3da9623b6c42bc5b2e6b909d62f6e431
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2024-12-08 17:03:06 +0900

    cors 설정 추가, OPTION 허용

--- src/main/java/com/example/accessingdatamysql/Security/SecurityConfig.java
+++ src/main/java/com/example/accessingdatamysql/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.accessingdatamysql.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,8 +17,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().permitAll(); // 모든 요청 허용
         return http.build();
     }
